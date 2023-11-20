dc7f4d70c8fa35e7afa8ca0f33be5149492af8da
abydon <bitcoin@abydon.com>
2023-11-20 09:58:31 +0100

    Update debloat_silent.ps1

--- debloat_silent.ps1
+++ debloat_silent.ps1
@@ -1,4 +1,5 @@
 # PowerShell-Skript zur Entfernung vorinstallierter Bloatware in Windows 10 basierend auf einer Whitelist
+# script design: I.Pielczyk für task INFRA-1848 - Windows 10: Vorinstallierte Apps deinstallieren
 
 # Überprüfen, ob das Skript bereits ausgeführt wurde
 $LogFile = "C:\Temp\RemoveApps_Log.txt"
@@ -7,61 +8,74 @@ if (Test-Path $LogFile) {
     exit
 }
 
-# Liste der Apps, die behalten werden sollen
+# Whitelist der Apps, die behalten werden sollen
 $Whitelist = @(
-    "*MicrosoftTeams*",
-    "*1527c705-839a-4832-9118-54d4Bd6a0c89*",
-    "*c5e2524a-ea46-4f67-841f-6a9465d9d515*",
-    "*E2A4F912-2574-4A75-9BB0-0D023378592B*",
-    "*F46D4000-FD22-4DB4-AC8E-4E1DDDE828FE*",
-    "*InputApp*",
-    "*Microsoft.AAD.BrokerPlugin*",
-    "*Microsoft.AccountsControl*",
-    "*Microsoft.BioEnrollment*",
-    "*Microsoft.CredDialogHost*",
-    "*Microsoft.ECApp*",
-    "*Microsoft.LockApp*",
-    "*Microsoft.MicrosoftEdgeDevToolsClient*",
-    "*Microsoft.MicrosoftEdge*",
-    "*Microsoft.PPIProjection*",
-    "*Microsoft.Win32WebViewHost*",
-    "*Microsoft.Windows.Apprep.ChxApp*",
-    "*Microsoft.Windows.AssignedAccessLockApp*",
-    "*Microsoft.Windows.CapturePicker*",
-    "*Microsoft.Windows.CloudExperienceHost*",
-    "*Microsoft.Windows.ContentDeliveryManager*",
-    "*Microsoft.Windows.Cortana*",
-    "*Microsoft.Windows.OOBENetworkCaptivePortal*",
-    "*Microsoft.Windows.OOBENetworkConnectionFlow*",
-    "*Microsoft.Windows.ParentalControls*",
-    "*Microsoft.Windows.PinningConfirmationDialog*",
-    "*Microsoft.Windows.SecHealthUI*",
-    "*Microsoft.Windows.SecureAssessmentBrowser*",
-    "*Microsoft.Windows.ShellExperienceHost*",
-    "*Microsoft.Windows.XGpuEjectDialog*",
-    "*Microsoft.XboxGameCallableUI*",
-    "*Windows.CBSPreview*",
-    "*windows.immersivecontrolpanel*",
-    "*Windows.PrintDialog*",
-    "*Microsoft.VCLibs.140.00*",
-    "*Microsoft.Services.Store.Engagement*",
-    "*Microsoft.UI.Xaml.2.0*",
-    "*Microsoft.WindowsCalculator*",
-    "*Microsoft.WindowsStore*",
-    "*Microsoft.Windows.Photos*",
-    "*CanonicalGroupLimited.UbuntuonWindows*",
-    "*Microsoft.MSPaint*",
-    "*Microsoft.WindowsCamera*",
-    "*\.NET*",
-    "*Microsoft.HEIFImageExtension*",
-    "*Microsoft.ScreenSketch*",
-    "*Microsoft.StorePurchaseApp*",
-    "*Microsoft.VP9VideoExtensions*",
-    "*Microsoft.WebMediaExtensions*",
-    "*Microsoft.WebpImageExtension*",
-    "*Microsoft.DesktopAppInstaller*",
-    "*Microsoft.RemoteDesktop*"
+    "Microsoft.Teams",
+    "1527c705-839a-4832-9118-54d4Bd6a0c89",
+    "c5e2524a-ea46-4f67-841f-6a9465d9d515",
+    "E2A4F912-2574-4A75-9BB0-0D023378592B",
+    "F46D4000-FD22-4DB4-AC8E-4E1DDDE828FE",
+    "InputApp",
+    "Microsoft.AAD.BrokerPlugin",
+    "Microsoft.AccountsControl",
+    "Microsoft.BioEnrollment",
+    "Microsoft.CredDialogHost",
+    "Microsoft.ECApp",
+    "Microsoft.LockApp",
+    "Microsoft.MicrosoftEdgeDevToolsClient",
+    "Microsoft.MicrosoftEdge",
+    "Microsoft.PPIProjection",
+    "Microsoft.Win32WebViewHost",
+    "Microsoft.Windows.Apprep.ChxApp",
+    "Microsoft.Windows.AssignedAccessLockApp",
+    "Microsoft.Windows.CapturePicker",
+    "Microsoft.Windows.CloudExperienceHost",
+    "Microsoft.Windows.ContentDeliveryManager",
+    "Microsoft.Windows.Cortana",
+    "Microsoft.Windows.HolographicFirstRun",
+    "Microsoft.Windows.NarratorQuickStart",
+    "Microsoft.Windows.OOBENetworkCaptivePortal",
+    "Microsoft.Windows.OOBENetworkConnectionFlow",
+    "Microsoft.Windows.ParentalControls",
+    "Microsoft.Windows.PeopleExperienceHost",
+    "Microsoft.Windows.PinningConfirmationDialog",
+    "Microsoft.Windows.SecHealthUI",
+    "Microsoft.Windows.SecondaryTileExperience",
+    "Microsoft.Windows.SecureAssessmentBrowser",
+    "Microsoft.Windows.ShellExperienceHost",
+    "Microsoft.Windows.XGpuEjectDialog",
+    "Microsoft.XboxGameCallableUI",
+    "Windows.CBSPreview",
+    "windows.immersivecontrolpanel",
+    "Windows.PrintDialog",
+    "Microsoft.VCLibs.140.00",
+    "Microsoft.Services.Store.Engagement",
+    "Microsoft.UI.Xaml.2.0",
+    "Microsoft.WindowsCalculator",
+    "Microsoft.WindowsStore",
+    "Microsoft.Windows.Photos",
+    "CanonicalGroupLimited.UbuntuonWindows",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.XboxApp",
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.MicrosoftStickyNotes",
+    "Microsoft.MSPaint",
+    "Microsoft.WindowsCamera",
+    ".NET",
+    "Microsoft.HEIFImageExtension",
+    "Microsoft.ScreenSketch",
+    "Microsoft.StorePurchaseApp",
+    "Microsoft.VP9VideoExtensions",
+    "Microsoft.WebMediaExtensions",
+    "Microsoft.WebpImageExtension",
+    "Microsoft.DesktopAppInstaller",
+    "Microsoft.RemoteDesktop",
+    "Microsoft.Windows.OneDrive"
 )
+
 
 # Alle vorinstallierten Apps abrufen
 $InstalledApps = Get-AppxPackage -AllUsers | Where-Object { $_.PackageFullName -notin $Whitelist }
@@ -79,7 +93,7 @@ $LogContent | Out-File -FilePath $LogFile
 # Apps entfernen, die nicht auf der Whitelist stehen
 foreach ($App in $InstalledApps) {
     Write-Host "Entferne $($App.Name)..." -NoNewline
-    Remove-AppxPackage -Package $App.PackageFullName -AllUsers -Quiet
+    Remove-AppxPackage -Package $App.PackageFullName -AllUsers -ErrorAction SilentlyContinue
     Write-Host "Fertig"
 }
 
@@ -90,11 +104,11 @@ $ProgramDirectories = @("C:\Program Files", "C:\Program Files (x86)")
 
 # Durchsuche die Verzeichnisse nach installierten Programmen
 foreach ($ProgramDirectory in $ProgramDirectories) {
-    $InstalledPrograms = Get-ChildItem $ProgramDirectory | Where-Object { $_.PSIsContainer } | ForEach-Object {
+    $InstalledPrograms = Get-ChildItem $ProgramDirectory -Directory | ForEach-Object {
         $_.Name
     }
 
-    Write-Host "Installierte Programme unter $($ProgramDirectory):"
+    Write-Host "Installierte Programme unter ${ProgramDirectory}:"
     $InstalledPrograms
     Write-Host ""
 }
