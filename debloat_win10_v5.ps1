9420a2eb5067720be1309c25fc7ecff08cf62fa9
abydon <bitcoin@abydon.com>
2023-11-21 22:44:08 +0100

    Update debloat_win10_v5.ps1

--- debloat_win10_v5.ps1
+++ debloat_win10_v5.ps1
@@ -8,59 +8,19 @@ if (Test-Path $LogFile) {
     exit
 }
 
-# Whitelist der Apps, die behalten werden sollen
+# Liste der Apps, die behalten werden sollen
 $Whitelist = @(
-    "Microsoft.Teams",
-    "1527c705-839a-4832-9118-54d4Bd6a0c89",
-    "c5e2524a-ea46-4f67-841f-6a9465d9d515",
-    "E2A4F912-2574-4A75-9BB0-0D023378592B",
-    "F46D4000-FD22-4DB4-AC8E-4E1DDDE828FE",
-    "InputApp",
-    "Microsoft.AAD.BrokerPlugin",
-    "Microsoft.AccountsControl",
-    "Microsoft.BioEnrollment",
-    "Microsoft.CredDialogHost",
-    "Microsoft.ECApp",
-    "Microsoft.LockApp",
-    "Microsoft.MicrosoftEdgeDevToolsClient",
-    "Microsoft.MicrosoftEdge",
-    "Microsoft.PPIProjection",
-    "Microsoft.Win32WebViewHost",
-    "Microsoft.Windows.Apprep.ChxApp",
-    "Microsoft.Windows.AssignedAccessLockApp",
-    "Microsoft.Windows.CapturePicker",
-    "Microsoft.Windows.CloudExperienceHost",
-    "Microsoft.Windows.ContentDeliveryManager",
-    "Microsoft.Windows.Cortana",
-    "Microsoft.Windows.HolographicFirstRun",
-    "Microsoft.Windows.NarratorQuickStart",
-    "Microsoft.Windows.OOBENetworkCaptivePortal",
-    "Microsoft.Windows.OOBENetworkConnectionFlow",
-    "Microsoft.Windows.ParentalControls",
-    "Microsoft.Windows.PeopleExperienceHost",
-    "Microsoft.Windows.PinningConfirmationDialog",
-    "Microsoft.Windows.SecHealthUI",
-    "Microsoft.Windows.SecondaryTileExperience",
-    "Microsoft.Windows.SecureAssessmentBrowser",
-    "Microsoft.Windows.ShellExperienceHost",
-    "Microsoft.Windows.XGpuEjectDialog",
-    "Microsoft.XboxGameCallableUI",
-    "Windows.CBSPreview",
-    "windows.immersivecontrolpanel",
-    "Windows.PrintDialog",
-    "Microsoft.VCLibs.140.00",
-    "Microsoft.Services.Store.Engagement",
-    "Microsoft.UI.Xaml.2.0",
+    "Microsoft.ScreenSketch",
+    "Microsoft.Paint3D",
     "Microsoft.WindowsCalculator",
     "Microsoft.WindowsStore",
     "Microsoft.Windows.Photos",
     "CanonicalGroupLimited.UbuntuonWindows",
+    "Microsoft.XboxGameCallableUI",
+    "Microsoft.XboxGamingOverlay",
     "Microsoft.Xbox.TCUI",
-    "Microsoft.XboxApp",
-    "Microsoft.XboxGameOverlay",
     "Microsoft.XboxGamingOverlay",
     "Microsoft.XboxIdentityProvider",
-    "Microsoft.XboxSpeechToTextOverlay",
     "Microsoft.MicrosoftStickyNotes",
     "Microsoft.MSPaint",
     "Microsoft.WindowsCamera",
@@ -72,12 +32,10 @@ $Whitelist = @(
     "Microsoft.WebMediaExtensions",
     "Microsoft.WebpImageExtension",
     "Microsoft.DesktopAppInstaller",
-    "Microsoft.RemoteDesktop",
-    "Microsoft.Windows.OneDrive"
+    "WindSynthBerry",
+    "MIDIBerry",
+    "Slack"
 )
-
-# Alle vorinstallierten Apps abrufen
-$InstalledApps = Get-AppxPackage -AllUsers | Where-Object { $_.PackageFullName -notin $Whitelist }
 
 # Überprüfen, ob das Verzeichnis für das Logfile existiert, andernfalls erstellen
 $LogDirectory = "C:\Temp"
@@ -87,29 +45,32 @@ if (-not (Test-Path $LogDirectory)) {
 
 # Logdatei erstellen
 $LogContent = "Bereinigungsdatum: $(Get-Date)"
-$LogContent | Out-File -FilePath $LogFile
+$LogContent | Out-File -FilePath $LogFile -Append
 
-# Apps entfernen, die nicht auf der Whitelist stehen
+# Alle vorinstallierten Apps abrufen
+$InstalledApps = Get-AppxPackage -AllUsers
+
+# Durchlaufe installierte Apps
 foreach ($App in $InstalledApps) {
-    Write-Host "Entferne $($App.Name)..." -NoNewline
-    Remove-AppxPackage -Package $App.PackageFullName -AllUsers -ErrorAction SilentlyContinue
-    Write-Host "Fertig"
+    $AppName = $App.Name
+    $PackageFullName = $App.PackageFullName
+
+    # Überprüfe, ob App in Whitelist
+    $IsWhitelisted = $Whitelist -contains $PackageFullName -or $Whitelist -contains $AppName
+
+    # Logge Information
+    "$AppName - Whitelisted: $IsWhitelisted" | Out-File -FilePath $LogFile -Append
+
+    # Falls nicht in Whitelist, entferne die App
+    if (-not $IsWhitelisted) {
+        Write-Host "Entferne $AppName..." -NoNewline
+        try {
+            Remove-AppxPackage -Package $PackageFullName -AllUsers -ErrorAction Stop
+            Write-Host "Fertig"
+        } catch {
+            Write-Host "Konnte nicht entfernt werden"
+        }
+    }
 }
 
 Write-Host "Whitelisted Apps bleiben erhalten. Andere wurden entfernt."
-
-# Verzeichnisse, in denen nach installierten Programmen gesucht werden soll
-$ProgramDirectories = @("C:\Program Files", "C:\Program Files (x86)")
-
-# Durchsuche die Verzeichnisse nach installierten Programmen
-foreach ($ProgramDirectory in $ProgramDirectories) {
-    $InstalledPrograms = Get-ChildItem $ProgramDirectory -Directory | ForEach-Object {
-        $_.Name
-    }
-
-    Write-Host "Installierte Programme unter ${ProgramDirectory}:"
-    $InstalledPrograms
-    Write-Host ""
-}
-
-Write-Host "Die Deinstallation dieser Programme erfolgt nicht automatisch. Dies dient nur zur Anzeige der installierten Programme."
